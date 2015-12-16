d12faca75adf5c520e3112f880f1b2fe89af01d7
tikomo <tikomo@gmail.com>
2015-12-16 11:48:12 +0900

    1st commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.pictureBoxIpl2 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.pictureBoxIpl3 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(8, 88);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBoxIpl1.TabIndex = 0;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "グレースケール";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(200, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "グレースケール";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(296, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 32);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "2値化";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(392, 8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 32);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "2値化";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(488, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 32);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "2値化";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(584, 8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(88, 32);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "2値化";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(8, 48);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 32);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "適応閾値処理";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(104, 48);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(88, 32);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "適応閾値処理";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(200, 48);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(88, 32);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "適応閾値処理";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(296, 48);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(88, 32);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "適応閾値処理";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(488, 48);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(88, 32);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "輪郭検出";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(392, 48);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(88, 32);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "2値化 反転";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(584, 48);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(88, 32);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "ラベリング";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // pictureBoxIpl2
+            // 
+            this.pictureBoxIpl2.Location = new System.Drawing.Point(336, 88);
+            this.pictureBoxIpl2.Name = "pictureBoxIpl2";
+            this.pictureBoxIpl2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBoxIpl2.TabIndex = 15;
+            this.pictureBoxIpl2.TabStop = false;
+            // 
+            // pictureBoxIpl3
+            // 
+            this.pictureBoxIpl3.Location = new System.Drawing.Point(8, 336);
+            this.pictureBoxIpl3.Name = "pictureBoxIpl3";
+            this.pictureBoxIpl3.Size = new System.Drawing.Size(320, 240);
+            this.pictureBoxIpl3.TabIndex = 16;
+            this.pictureBoxIpl3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 586);
+            this.Controls.Add(this.pictureBoxIpl3);
+            this.Controls.Add(this.pictureBoxIpl2);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl2;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using OpenCvSharp;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 元画像の表示
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            this.pictureBoxIpl1.ImageIpl = src;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 元画像の表示
+            Mat src = new Mat("Images/testimage.png", ImreadModes.GrayScale);
+            this.pictureBoxIpl1.ImageIpl = src;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // グレースケール
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+
+            this.pictureBoxIpl1.ImageIpl = gray;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // ２値化 大津式
+            Mat src = new Mat("Images/testimage.png", ImreadModes.GrayScale);
+
+            Mat binary = src.Threshold(0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
+            this.pictureBoxIpl1.ImageIpl = binary;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // グレースケール
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+         
+            // ２値化 大津式
+            Mat binary = gray.Threshold(0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
+            this.pictureBoxIpl1.ImageIpl = binary;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // 閾値を指定します
+            int threshold = 128;
+
+            Mat gray = new Mat("Images/testimage.png", ImreadModes.GrayScale);
+            Mat binary = gray.Threshold(threshold, 255, ThresholdTypes.Binary);
+
+            this.pictureBoxIpl1.ImageIpl = binary;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // 閾値を指定します
+            int threshold = 64;
+
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+
+            Mat binary = gray.Threshold(threshold, 255, ThresholdTypes.Binary);
+
+            this.pictureBoxIpl1.ImageIpl = binary;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // 適応的閾値処理
+            // やっぱり文字認識用かな
+            Mat src = new Mat("Images/testimage.png", ImreadModes.GrayScale);
+            Mat mat1 = src.AdaptiveThreshold(255, AdaptiveThresholdTypes.GaussianC, ThresholdTypes.Binary, 9, 12);
+            this.pictureBoxIpl1.ImageIpl = mat1;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // 適応的閾値処理
+            // やっぱり文字認識用かな
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+            Mat mat1 = gray.AdaptiveThreshold(255, AdaptiveThresholdTypes.GaussianC, ThresholdTypes.Binary, 9, 12);
+            this.pictureBoxIpl1.ImageIpl = mat1;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // 適応的閾値処理
+            // やっぱり文字認識用かな
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+            Mat mat1 = gray.AdaptiveThreshold(255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.Binary, 9, 12);
+            this.pictureBoxIpl1.ImageIpl = mat1;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // 適応的閾値処理
+            // やっぱり文字認識用かな
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+            Mat mat1 = gray.AdaptiveThreshold(255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.Binary, 9, 12);
+            this.pictureBoxIpl1.ImageIpl = mat1;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            // 輪郭検出
+
+            // 閾値固定
+            int threshold = 128;
+
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color); // カラー
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+
+            // 黒ベースの白のオブジェクトにしておく
+            Mat binary = gray.Threshold(threshold, 255, ThresholdTypes.BinaryInv);
+
+            OpenCvSharp.Point[][] ss;
+
+            ss = binary.Clone().FindContoursAsArray(RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+            int num = 0;
+            for (int i = 0; i < ss.Length; i++)
+            {
+
+                num++;
+                //
+                // 2値化後の画像に描いても、白または黒になってしまいます。
+                // 元の画像に描く方が分かりやすいです。
+                // ここまで作って、検査時に使った元画像なんて破壊されても別に問題ないじゃんと思ってしまった
+                //
+                src.DrawContours(ss, i, Scalar.Aqua, 4, LineTypes.AntiAlias);
+
+            }
+
+            this.pictureBoxIpl1.ImageIpl = src;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            // グレースケール
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color);
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+
+            // ２値化 大津式
+            // 黒ベースの白のオブジェクトにしておく
+            Mat binary = gray.Threshold(0, 255, ThresholdTypes.Otsu | ThresholdTypes.BinaryInv);
+            this.pictureBoxIpl1.ImageIpl = binary;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            // ラベリング
+
+            // 閾値指定
+            int threshold = 128;
+
+            Mat src = new Mat("Images/testimage.png", ImreadModes.Color); // カラー
+            Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+
+            // ここで反転させたけど、これは元画像の状態によるので注意してね
+            // 黒背景で白物体にしたいだけだから
+            Mat binary = gray.Threshold(threshold, 255, ThresholdTypes.BinaryInv);
+            
+            Mat labelView = src.EmptyClone();
+            Mat rectView = binary.CvtColor(ColorConversionCodes.GRAY2BGR);
+
+            ConnectedComponents cc = Cv2.ConnectedComponentsEx(binary);
+            if (cc.LabelCount <= 1)
+            {
+                // 見つからないね
+                // ここで抜ける処理入れてね
+
+                Debug.WriteLine("All Label = " + cc.LabelCount.ToString());
+            }
+
+            // draw labels
+            cc.RenderBlobs(labelView);
+
+            int num = 0;
+
+            foreach (var blob in cc.Blobs.Skip(1))
+            {
+                int area = blob.Area;
+
+                //if ((250 < area) && (area < 10000)) // 一定量を制限する場合
+                //{
+                    Debug.WriteLine(area.ToString());
+                    num++;
+                    rectView.Rectangle(blob.Rect, Scalar.Red);
+                //}
+            }
+            Debug.WriteLine("num = " + num.ToString());
+
+            // filter maximum blob
+            var maxBlob = cc.GetLargestBlob();
+            var filtered = new Mat();
+            cc.FilterByBlob(src, filtered, maxBlob);
+
+            this.pictureBoxIpl1.ImageIpl = binary;
+            this.pictureBoxIpl2.ImageIpl = labelView;
+            this.pictureBoxIpl3.ImageIpl = rectView;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.pictureBoxIpl1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.pictureBoxIpl2.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.pictureBoxIpl3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
